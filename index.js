fe32bc32b6830eaf27c661e249401466a91a72f1
William Bartholomew <wdb@willbar.com>
2016-03-31 08:11:46 -0700

    Refactor and add QUIT and SAVE commands

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var redis = require('redis');
 var process = require('process');
-var readline = require('readline');
+var fs = require('fs');
 var commandLineArgs = require('command-line-args');
+var readline = require('readline');
 var split = require('argv-split');
-var commands = require('redis-commands');
+var redis = require('redis');
+var redisCommands = require('redis-commands');
 
 var options = getOptions();
 var client = createRedisClient(options);
+var commands = getCommands();
+var lastReply;
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -20,19 +23,12 @@ rl.on('line', (line) => {
 
   var commandName = command.length === 0 ? 'NOOP' : command[0].toUpperCase();
   var commandArgs = command.splice(1);
-  var commandFunc;
-
-  if (commandName === 'HELP') {
-    commandFunc = helpCommand;
-  } else if (commandName === 'NOOP') {
-    commandFunc = noopCommand;
-  } else {
-    commandFunc = client.send_command.bind(client);
-  }
+  var commandFunc = commands[commandName] || commands['NOOP'];
 
   commandFunc(commandName, commandArgs, (err, reply) => {
     if (err) console.error(err);
     if (reply) console.dir(reply);
+    lastReply = reply;
     rl.prompt();
   });
 }).on('close', () => {
@@ -60,11 +56,24 @@ function createRedisClient(options) {
   return client;
 }
 
+function getCommands() {
+  var commands = {};
+  for (var i = 0; i < redisCommands.list.length; i++) {
+    commands[redisCommands.list[i].toUpperCase()] = client.send_command.bind(client);
+  }
+  commands['HELP'] = helpCommand;
+  commands['NOOP'] = noopCommand;
+  commands['EXIT'] = quitCommand;
+  commands['QUIT'] = quitCommand;
+  commands['SAVE'] = saveCommand;
+  return commands;
+}
+
 function helpCommand(name, args, callback) {
   var matchingCommands = [];
-  for (var i = 0; i < commands.list.length; i++) {
-    if (args.length === 0 || commands.list[i].toUpperCase().indexOf(args[0].toUpperCase()) > -1) {
-      matchingCommands.push(commands.list[i].toUpperCase());
+  for (var i = 0; i < redisCommands.list.length; i++) {
+    if (args.length === 0 || redisCommands.list[i].toUpperCase().indexOf(args[0].toUpperCase()) > -1) {
+      matchingCommands.push(redisCommands.list[i].toUpperCase());
     }
   }
   callback(null, matchingCommands);
@@ -72,4 +81,17 @@ function helpCommand(name, args, callback) {
 
 function noopCommand(name, args, callback) {
   callback(null, null);
+}
+
+function quitCommand(name, args, callback) {
+  if (client) client.unref();
+  process.exit(0);
+}
+
+function saveCommand(name, args, callback) {
+  if (args.length !== 1) return callback('SAVE filename', null);
+  if (!lastReply) return callback('No reply to save', null);
+  var filename = args[0];
+  fs.writeFileSync(filename, JSON.stringify(lastReply));
+  callback(null, 'Saved last reply to ' + filename);
 }
